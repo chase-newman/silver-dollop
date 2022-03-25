c709918b3894e7e1beeb1a58969e60f29b63245e
Chase Newman <chasejn01@gmail.com>
2022-03-25 16:38:17 -0400

    update fetch request

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const btn = document.querySelector("button");
 const anchorTag = document.querySelector("a");
+const input = document.querySelector("input");
 
-btn.addEventListener("click", function() {
+
+btn.addEventListener("click",(event) => {
+    console.log(event);
     console.log("Button Clicked");
 });
 
+input.addEventListener("keydown", (e) => {
+   console.log(e.key);
+   console.log(e.code);
+});
+
+// window.addEventListener("keydown", function(e) {
+//   console.log(e.key);
+//   console.log(e.code);
+// });
+
+// fetch("https://data.sec.gov/api/xbrl/companyfacts/CIK0000320193.json")
+//     .then(response => {
+//         console.log(response);
+//     })
+    
+// fetch("https://data.sec.gov/submissions/CIK0000320193.json")
+//     .then(response => {
+//       console.log(response) 
+//     });
 
 
-fetch("https://test-project-d14cc-default-rtdb.firebaseio.com/users.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.website);
-        const linkSource = data.website;
-        console.log(linkSource, anchorTag.href);
-        anchorTag.href = linkSource
-    });
+
+// fetch("https://test-project-d14cc-default-rtdb.firebaseio.com/users.json")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.website);
+//         const linkSource = data.website;
+//         console.log(linkSource, anchorTag.href);
+//         anchorTag.href = linkSource
+//     });
     
     
     
