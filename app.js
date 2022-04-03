7cebd70f48bad2e2de1f36da7e5fb1ac96038c5a
Chase Newman <chasejn01@gmail.com>
2022-04-03 13:13:53 -0400

    add additional stock data

--- app.js
+++ app.js
@@ -1,56 +1,141 @@
 let dataSet = [];
 
-fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=1D24JWD6HONH93GD")
+const labels = [29,28,27,26,25,24,23,22,21,20,
+                19,18,17,16,15,14,13,12,10,9,8,
+                7,6,5,4,3,2,1,0];
+                
+let myChart
+
+let dataY = {
+        labels: labels,
+        datasets: [{
+          label: "Stock Price ($USD)",
+          backgroundColor: 'rgb(2, 252, 135)',
+          borderColor: 'rgb(2, 252, 135)',
+          data: dataSet,
+          fill: true
+        }]
+    };
+
+let config = {
+        type: 'line',
+        data: dataY,
+        options: {
+            animations: {
+                tension: {
+                    duration: 1000,
+                    easing: 'linear',
+                    from: 1,
+                    to: 0,
+                    loop: false
+                }
+            }
+        }
+      };
+
+const stockArr = [
+        {
+            name: "Apple",
+            ticker: "AAPL"
+        },
+        {
+            name: "Amazon",
+            ticker: "AMZN"
+        }
+    ];
+
+fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[0].ticker}&apikey=1D24JWD6HONH93GD`)
+    .then(response => response.json())
+    .then(data => {
+        let newData = [];
+    
+        const stockPriceObject = Object.values(data)[1];
+        let stockPriceArr = Object.values(stockPriceObject);
+        const todaysStockPriceValues = stockPriceArr[0];
+        const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
+        // console.log(dataFour.toFixed(2));
+        document.querySelector("#price").innerHTML = closingStockPrice;
+        console.log(document.querySelectorAll(".stockName"));
+        const stockNameElements = document.querySelectorAll(".stockName");
+        for(let el of stockNameElements) {
+            el.innerHTML = stockArr[0].name;
+        }
+        for(let i=0; i<30; i++) {
+            newData.push(parseFloat(Object.values(stockPriceArr[i])[3]));
+        }
+        if(newData.length > 0) {
+            dataX = {
+                labels: labels,
+                datasets: [{
+                  label: "Stock Price ($USD)",
+                  backgroundColor: 'rgb(2, 135, 90)',
+                  borderColor: 'rgb(2, 135, 90)',
+                  data: newData,
+                  fill: true
+                }]
+            };
+            
+            config = {
+                type: 'line',
+                data: dataX,
+                options: {
+                    animations: {
+                        tension: {
+                            duration: 1000,
+                            easing: 'linear',
+                            from: 1,
+                            to: 0,
+                            loop: false
+                        }
+                    }
+                }
+            };
+            myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );  
+}
+}).catch((e) => {
+   console.log(e); 
+});
+
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+   console.log("button clicked");
+   myChart.destroy();
+   dataSet = [];
+   
+   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[1].ticker}&apikey=1D24JWD6HONH93GD`)
     .then(response => response.json())
     .then(data => {
         const stockPriceObject = Object.values(data)[1];
         let stockPriceArr = Object.values(stockPriceObject);
-        console.log(stockPriceArr);
-
         const todaysStockPriceValues = stockPriceArr[0];
         const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
         // console.log(dataFour.toFixed(2));
-        document.querySelector("span").innerHTML = closingStockPrice;
+        document.querySelector("#price").innerHTML = closingStockPrice;
+        console.log(document.querySelectorAll(".stockName"));
+        const stockNameElements = document.querySelectorAll(".stockName");
+        for(let el of stockNameElements) {
+            el.innerHTML = stockArr[0].name;
+        }
         for(let i=0; i<30; i++) {
-            console.log(parseFloat(Object.values(stockPriceArr[i])[3]));
             dataSet.push(parseFloat(Object.values(stockPriceArr[i])[3]));
-            console.log(dataSet);
         }
-         
-  const labels = [29,28,27,26,25,24,23,22,21,20,
-                19,18,17,16,15,14,13,12,10,9,8,
-                7,6,5,4,3,2,1,0];
-
-  const dataY = {
-    labels: labels,
-    datasets: [{
-      label: "Stock Price ($USD)",
-      backgroundColor: 'rgb(2, 252, 135)',
-      borderColor: 'rgb(2, 252, 135)',
-      data: dataSet,
-      fill: true
-    }]
-  };
-  const config = {
-    type: 'line',
-    data: dataY,
-    options: {
-        animations: {
-            tension: {
-                duration: 1000,
-                easing: 'linear',
-                from: 1,
-                to: 0,
-                loop: false
-            }
-        }
-    }
-  };
-  const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    ); 
+        if(dataSet.length > 0) {
+            myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );  
+}
+}).catch((e) => {
+   console.log(e); 
 });
+   
+});
+
     
 
 
