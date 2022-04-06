bfb44e489e2fc93fc38569f5f4a61854dd1a9056
Chase Newman <chasejn01@gmail.com>
2022-04-06 07:12:37 -0400

    refactor API call

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,15 @@
-const stockArr = [
-        {
-            name: "Apple",
-            ticker: "AAPL"
-        },
-        {
-            name: "Amazon",
-            ticker: "AMZN"
-        },
-        {
-            name: "Netflix",
-            ticker: "NFLX"
-        },
-        {
-            name: "Meta",
-            ticker: "FB"
-        },
-        {
-            name: "Google",
-            ticker: "GOOG"
-        }
-    ];
+//Declare stock variables and select button elements
+const appleStock = {name: "Apple", ticker: "AAPL", color: "rgb(255, 202, 44)"};
+const amazonStock = {name: "Amazon", ticker: "AMZN", color: "rgb(2, 135, 90)"};
+const netflixStock = {name: "Netflix", ticker: "NFLX", color: "rgb(255, 0, 0)"};
+const metaStock = {name: "Meta", ticker: "FB", color: "rgb(11, 94, 215)"};
+const googleStock = {name: "Google", ticker: "GOOG", color: "rgb(92, 99, 106)"};
+    
+const appleBtn = document.querySelector("#apple-btn");
+const amznBtn = document.querySelector("#amazon-btn");
+const nflxBtn = document.querySelector("#netflix-btn");
+const metaBtn = document.querySelector("#meta-btn");
+const googBtn = document.querySelector("#google-btn");
 
 let dataSet = [];
 
@@ -30,36 +20,9 @@ const labels = [29,28,27,26,25,24,23,22,21,20,
                 7,6,5,4,3,2,1,0];
                 
 
-// let dataY = {
-//         labels: labels,
-//         datasets: [{
-//           label: "Stock Price ($USD)",
-//           backgroundColor: 'rgb(2, 252, 135)',
-//           borderColor: 'rgb(2, 252, 135)',
-//           data: dataSet,
-//           fill: true
-//         }]
-//     };
-
-// let config = {
-//         type: 'line',
-//         data: dataY,
-//         options: {
-//             animations: {
-//                 tension: {
-//                     duration: 1000,
-//                     easing: 'linear',
-//                     from: 1,
-//                     to: 0,
-//                     loop: false
-//                 }
-//             }
-//         }
-//       };
-
-
-
-fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[0].ticker}&apikey=1D24JWD6HONH93GD`)
+//create API function call
+const getStockData = (stock) => {
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock.ticker}&apikey=1D24JWD6HONH93GD`)
     .then(response => response.json())
     .then(data => {
         const stockPriceObject = Object.values(data)[1];
@@ -67,10 +30,9 @@ fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${sto
         const todaysStockPriceValues = stockPriceArr[0];
         const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
         document.querySelector("#price").innerHTML = closingStockPrice;
-        console.log(document.querySelectorAll(".stockName"));
         const stockNameElements = document.querySelectorAll(".stockName");
         for(let el of stockNameElements) {
-            el.innerHTML = stockArr[0].name;
+            el.innerHTML = stock.name;
         }
         for(let i=0; i<30; i++) {
             dataSet.push(parseFloat(Object.values(stockPriceArr[i])[3]));
@@ -80,8 +42,8 @@ fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${sto
                 labels: labels,
                 datasets: [{
                   label: "Stock Price ($USD)",
-                  backgroundColor: 'rgb(255, 202, 44)',
-                  borderColor: 'rgb(255, 202, 44)',
+                  backgroundColor: stock.color,
+                  borderColor: stock.color,
                   data: dataSet,
                   fill: true
                 }]
@@ -106,270 +68,63 @@ fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${sto
             document.getElementById('myChart'),
             config
         );  
-}
+    }
 }).catch((e) => {
-   console.log(e); 
+       console.log(e); 
+    });
+};
+
+getStockData(appleStock);
+
+
+
+//get new stock data with button clicks
+
+appleBtn.addEventListener("click", () => {
+   //Destroy the previous chart and empty the dataset
+   myChart.destroy();
+   dataSet = [];
+
+   //Get Amazon stock data 
+   getStockData(appleStock); 
 });
-
-
-const amznBtn = document.querySelector("#amazon-btn");
-const nflxBtn = document.querySelector("#netflix-btn");
-const metaBtn = document.querySelector("#meta-btn");
-const googBtn = document.querySelector("#google-btn");
 
 
 amznBtn.addEventListener("click", () => {
-   console.log("button clicked");
    //Destroy the previous chart and empty the dataset
    myChart.destroy();
    dataSet = [];
-   
-   //Fetch Amazon stock data
-   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[1].ticker}&apikey=1D24JWD6HONH93GD`)
-    .then(response => response.json())
-    .then(data => {
-        const stockPriceObject = Object.values(data)[1];
-        let stockPriceArr = Object.values(stockPriceObject);
-        const todaysStockPriceValues = stockPriceArr[0];
-        const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
-        document.querySelector("#price").innerHTML = closingStockPrice;
-        const stockNameElements = document.querySelectorAll(".stockName");
-        for(let el of stockNameElements) {
-            el.innerHTML = stockArr[1].name;
-        }
-        for(let i=0; i<30; i++) {
-            dataSet.push(parseFloat(Object.values(stockPriceArr[i])[3]));
-        }
-        
-        //create new chart
-        if(dataSet.length > 0) {
-            
-            dataX = {
-                labels: labels,
-                datasets: [{
-                  label: "Stock Price ($USD)",
-                  backgroundColor: 'rgb(2, 135, 90)',
-                  borderColor: 'rgb(2, 135, 90)',
-                  data: dataSet,
-                  fill: true
-                }]
-            };
-            
-            config = {
-                type: 'line',
-                data: dataX,
-                options: {
-                    animations: {
-                        tension: {
-                            duration: 1000,
-                            easing: 'linear',
-                            from: 1,
-                            to: 0,
-                            loop: false
-                        }
-                    }
-                }
-            };
-            myChart = new Chart(
-            document.getElementById('myChart'),
-            config
-        );  
-}
-}).catch((e) => {
-   console.log(e);
-   
-});
-   
+
+   //Get Amazon stock data 
+   getStockData(amazonStock);
 });
 
 nflxBtn.addEventListener("click", () => {
-   console.log("button clicked");
    //Destroy the previous chart and empty the dataset
    myChart.destroy();
    dataSet = [];
    
-   //Fetch Amazon stock data
-   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[2].ticker}&apikey=1D24JWD6HONH93GD`)
-    .then(response => response.json())
-    .then(data => {
-        const stockPriceObject = Object.values(data)[1];
-        let stockPriceArr = Object.values(stockPriceObject);
-        const todaysStockPriceValues = stockPriceArr[0];
-        const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
-        document.querySelector("#price").innerHTML = closingStockPrice;
-        const stockNameElements = document.querySelectorAll(".stockName");
-        for(let el of stockNameElements) {
-            el.innerHTML = stockArr[2].name;
-        }
-        for(let i=0; i<30; i++) {
-            dataSet.push(parseFloat(Object.values(stockPriceArr[i])[3]));
-        }
-        
-        //create new chart
-        if(dataSet.length > 0) {
-            
-            dataX = {
-                labels: labels,
-                datasets: [{
-                  label: "Stock Price ($USD)",
-                  backgroundColor: 'rgb(255, 0, 0)',
-                  borderColor: 'rgb(255, 0, 0)',
-                  data: dataSet,
-                  fill: true
-                }]
-            };
-            
-            config = {
-                type: 'line',
-                data: dataX,
-                options: {
-                    animations: {
-                        tension: {
-                            duration: 1000,
-                            easing: 'linear',
-                            from: 1,
-                            to: 0,
-                            loop: false
-                        }
-                    }
-                }
-            };
-            myChart = new Chart(
-            document.getElementById('myChart'),
-            config
-        );  
-}
-}).catch((e) => {
-   console.log(e); 
-});
-   
+   //Get Netflix stock data
+   getStockData(netflixStock);
 });
 
 metaBtn.addEventListener("click", () => {
-   console.log("button clicked");
    //Destroy the previous chart and empty the dataset
    myChart.destroy();
    dataSet = [];
    
-   //Fetch Amazon stock data
-   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[3].ticker}&apikey=1D24JWD6HONH93GD`)
-    .then(response => response.json())
-    .then(data => {
-        const stockPriceObject = Object.values(data)[1];
-        let stockPriceArr = Object.values(stockPriceObject);
-        const todaysStockPriceValues = stockPriceArr[0];
-        const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
-        document.querySelector("#price").innerHTML = closingStockPrice;
-        const stockNameElements = document.querySelectorAll(".stockName");
-        for(let el of stockNameElements) {
-            el.innerHTML = stockArr[3].name;
-        }
-        for(let i=0; i<30; i++) {
-            dataSet.push(parseFloat(Object.values(stockPriceArr[i])[3]));
-        }
-        
-        //create new chart
-        if(dataSet.length > 0) {
-            
-            dataX = {
-                labels: labels,
-                datasets: [{
-                  label: "Stock Price ($USD)",
-                  backgroundColor: 'rgb(11, 94, 215)',
-                  borderColor: 'rgb(11, 94, 215)',
-                  data: dataSet,
-                  fill: true
-                }]
-            };
-            
-            config = {
-                type: 'line',
-                data: dataX,
-                options: {
-                    animations: {
-                        tension: {
-                            duration: 1000,
-                            easing: 'linear',
-                            from: 1,
-                            to: 0,
-                            loop: false
-                        }
-                    }
-                }
-            };
-            myChart = new Chart(
-            document.getElementById('myChart'),
-            config
-        );  
-}
-}).catch((e) => {
-   console.log(e); 
-});
-   
+   //Get Meta stock data
+   getStockData(metaStock);
 });
 
 
 googBtn.addEventListener("click", () => {
-   console.log("button clicked");
    //Destroy the previous chart and empty the dataset
    myChart.destroy();
    dataSet = [];
    
-   //Fetch Amazon stock data
-   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockArr[4].ticker}&apikey=1D24JWD6HONH93GD`)
-    .then(response => response.json())
-    .then(data => {
-        const stockPriceObject = Object.values(data)[1];
-        let stockPriceArr = Object.values(stockPriceObject);
-        const todaysStockPriceValues = stockPriceArr[0];
-        const closingStockPrice = parseFloat(Object.values(todaysStockPriceValues)[3]);
-        document.querySelector("#price").innerHTML = closingStockPrice;
-        const stockNameElements = document.querySelectorAll(".stockName");
-        for(let el of stockNameElements) {
-            el.innerHTML = stockArr[4].name;
-        }
-        for(let i=0; i<30; i++) {
-            dataSet.push(parseFloat(Object.values(stockPriceArr[i])[3]));
-        }
-        
-        //create new chart
-        if(dataSet.length > 0) {
-            
-            dataX = {
-                labels: labels,
-                datasets: [{
-                  label: "Stock Price ($USD)",
-                  backgroundColor: 'rgb(92, 99, 106)',
-                  borderColor: 'rgb(92, 99, 106)',
-                  data: dataSet,
-                  fill: true
-                }]
-            };
-            
-            config = {
-                type: 'line',
-                data: dataX,
-                options: {
-                    animations: {
-                        tension: {
-                            duration: 1000,
-                            easing: 'linear',
-                            from: 1,
-                            to: 0,
-                            loop: false
-                        }
-                    }
-                }
-            };
-            myChart = new Chart(
-            document.getElementById('myChart'),
-            config
-        );  
-}
-}).catch((e) => {
-   console.log(e); 
-});
-   
+   //Get Google Stock data
+   getStockData(googleStock);
 });
     
 
